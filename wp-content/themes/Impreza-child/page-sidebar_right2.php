2b94ccc29cbd99d56df45d8eb9bc1f2b9a846c26
thomas.varga <thomas.varga@stendahls.se>
2015-01-24 12:08:55 +0100

    Inserted about festivale side menu

--- wp-content/themes/Impreza-child/page-sidebar_right2.php
+++ wp-content/themes/Impreza-child/page-sidebar_right2.php
@@ -8,17 +8,43 @@ define('SIDEBAR_POS', 'right');
 get_header();
 global $smof_data, $us_shortcodes;
 function get_sidebar_menu() {
-	global $post;
-	if ( is_page() && $post->post_parent )
-		$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
-	else
-		$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+
+	$page_id  = get_queried_object_id(); // Get current page id
+	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $page_id . '&echo=0' );
+
+	if($childpages)
+		return '<h6>'.get_the_title().'</h6><ul>' . $childpages . '</ul>';
+
+	$args = array(
+		'sort_order' => 'ASC',
+		'sort_column' => 'post_title',
+		'hierarchical' => 1,
+		'exclude' => '',
+		'include' => $page_id,
+		'meta_key' => '',
+		'meta_value' => '',
+		'authors' => '',
+		'child_of' => 0,
+		'parent' => -1,
+		'exclude_tree' => '',
+		'number' => '',
+		'offset' => 0,
+		'post_type' => 'page',
+		'post_status' => 'publish'
+	);
+	$pages = get_pages($args);
+
+	$parentID = $pages[0]->post_parent; // Get current page parent id
+
+	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $parentID . '&echo=0' );
+
 	if ( $childpages ) {
-		$string = '<ul>' . $childpages . '</ul>';
+		$string = '<h6>'. get_the_title($parentID) .'</h6><ul>' . $childpages . '</ul>';
 	}
 	return $string;
 }
 ?>
+
 <?php if (have_posts()) { while(have_posts()) { the_post(); ?>
 	<?php get_template_part( 'templates/pagehead' ); ?>
 	<div class="l-submain">
