703696bf00be250dc8fc4ae0447df639f98517ac
thomas.varga <thomas.varga@stendahls.se>
2015-01-24 12:15:41 +0100

    Removed ALL comments

---
+++ wp-content/themes/Impreza-child/comments.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+	die ('Please do not load this page directly. Thanks!');
+
+if ( post_password_required() ) {
+	return;
+}
+?>
